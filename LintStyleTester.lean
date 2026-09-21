e933aaf55f6fc18d2ca71af1248164b12292f154
Anne C.A. Baanen <vierkantor@vierkantor.com>
2026-09-21 18:19:24 +0200

    Initial commit: one file with trailing whitespace

---
+++ LintStyleTester.lean
@@ -0,0 +1,7 @@
+-- This module serves as the root of the `LintStyleTester` library.
+-- Import modules here that should be built as part of the library.
+import LintStyleTester.Basic
+
+def trailingWhitespace : Nat := 1          
+
+-- Comment with trailing whitespace           
---
+++ README.md
@@ -0,0 +1,3 @@
+# lint-style-tester
+
+Tests for the https://github.com/leanprover-community/lint-style-action
